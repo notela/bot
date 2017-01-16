9f82235f04fdd4e76db246be7a03050a8ce704fc
ToOfan <toofaneariyayi@gmail.com>
2017-01-17 01:02:24 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -79,36 +79,12 @@ function create_config( )
     "plugins",
     "tools"
  },
-    sudo_users = {157059515},
+    sudo_users = {111334847},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
-    info_text = [[》Beyond Reborn v1
-An advanced administration bot based on https://valtman.name/telegram-cli
-
-》https://github.com/BeyondTeam/BDReborn 
-
-》Admins :
-》@SoLiD ➣ Founder & Developer《
-》@Makan ➣ Developer《
-》@Rixel ➣ Developer 《
-》@CiveY ➣ Developer
-》@ArmanDev ➣ Manager《
-》@MrPars ➣ Manager《
-
-》Special thanks to :
-》@Vysheng
-》@MrHalix
-》@K_a_I_i_I_i_n_u_x
-》@Nero_Dev
-》And Beyond Team Members
-
-》Our channel :
-》@BeyondTeam《
-
-》Our website :
-》http://BeyondTeam.ir
-]],
+    info_text = [[Support & Coding by 		
+@To0fan]],
   }
   serialize_to_file(config, './data/config.lua')
   print ('saved config into conf.lua')
